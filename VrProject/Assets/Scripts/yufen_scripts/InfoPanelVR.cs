bde0e79d43a67c6e2156a270a870cbab1d0559d7
yayuli <pengyufentf@outlook.com>
2024-07-17 00:06:26 -0600

    made info UI prefab and script

---
+++ VrProject/Assets/Scripts/yufen_scripts/InfoPanelVR.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class InfoPanelVR : MonoBehaviour
+{
+    [SerializeField] private float offset = 1.1f;
+    [SerializeField] private Transform headTransform;
+    [SerializeField] private Vector3 localScale = Vector3.one;
+    [SerializeField] private GameObject infoUI;
+    [SerializeField] private float displayDuration = 5.0f; 
+
+    public bool isShown = false;
+    private XRGrabInteractable interactor;
+    private Coroutine hideUICoroutine;
+
+    private void Awake()
+    {
+        interactor = GetComponentInParent<XRGrabInteractable>();
+        if (interactor != null)
+        {
+            interactor.selectEntered.AddListener(OnSelectEnter);
+            Debug.Log("Event listener added to XRGrabInteractable.");
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (interactor != null)
+        {
+            interactor.selectEntered.RemoveListener(OnSelectEnter);
+        }
+    }
+
+    private void OnSelectEnter(SelectEnterEventArgs args)
+    {
+        if (isShown)
+        {
+            StopCoroutine(hideUICoroutine); 
+            infoUI.SetActive(false); 
+            isShown = false;
+        }
+
+        ShowInfo(args.interactable.transform); 
+    }
+
+    public void ShowInfo(Transform selectableTransform)
+    {
+        Debug.Log("DisplayInfoPanel triggered");
+
+        infoUI.SetActive(true);
+        isShown = true;
+
+        infoUI.transform.position = selectableTransform.position + Vector3.up * offset;
+       
+
+        
+        hideUICoroutine = StartCoroutine(HideInfoAfterTime(displayDuration));
+    }
+
+    private IEnumerator HideInfoAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        if (infoUI.activeSelf)
+        {
+            infoUI.SetActive(false);
+            isShown = false;
+            Debug.Log("Info panel automatically hidden after timeout.");
+        }
+    }
+}
